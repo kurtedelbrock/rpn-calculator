14773b924fbaa5e1e0dc98d2a25acb2981ee70e3
Kurt Edelbrock <kurt.edelbrock@careerbuilder.com>
2016-03-08 11:07:45 -0500

    Created a sorting hat to figure out where operands and operators should live at Hogwarts.

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,15 @@
 require_relative 'lib/calculator.rb'
 
 class CalculatorApp
+  attr_accessor :operands
+  attr_accessor :operators
 
+  def initialize
+    @operands = []
+    @operators = []
+  end
+
+  def add(input)
+    input =~ /[\+\-\*\/]/ ? @operators << input.to_sym : @operands << input
+  end
 end
--- spec/calculator_app_spec.rb
+++ spec/calculator_app_spec.rb
@@ -1,5 +1,19 @@
 require_relative '../calculator.rb'
 
 describe CalculatorApp do
-  
+  let(:app) { CalculatorApp.new }
+  describe "#add" do
+    context 'with a number' do
+      it 'should store it to the operands array' do
+        app.add 2
+        expect(app.operands).to include(2)
+      end
+    end
+    context 'with a operator' do
+      it 'should store it to the operators array' do
+        app.add "*"
+        expect(app.operators).to include(:*)
+      end
+    end
+  end
 end
