7b53402418250427bea768eccf251ace60eec479
Kurt Edelbrock <kurt.edelbrock@careerbuilder.com>
2016-03-08 10:30:48 -0500

    Adding tests for subtraction, multiplication, and division. Also made the second operand convert to a float in the calculate method so it returns floats and not integers.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,5 @@
 class Calculator
   def self.calculate_pair(first_operand, second_operand, operator)
-    first_operand.send operator, second_operand
+    first_operand.send operator, second_operand.to_f
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -5,9 +5,23 @@ describe Calculator do
     context 'with 3 and 4 as operands' do
       let(:first_operand) { 3 }
       let(:second_operand) { 4 }
+
       it 'should be able to add' do
-        expect(Calculator.calculate_pair(first_operand, second_operand, :+)).to be(7)
+        expect(Calculator.calculate_pair(first_operand, second_operand, :+)).to eq(7)
       end
+
+      it 'should be able to subtract' do
+        expect(Calculator.calculate_pair(first_operand, second_operand, :-)).to eq(-1)
+      end
+
+      it 'should be able to multiply' do
+        expect(Calculator.calculate_pair(first_operand, second_operand, :*)).to eq(12)
+      end
+
+      it 'should be able to divide' do
+        expect(Calculator.calculate_pair(first_operand, second_operand, :/)).to eq(0.75)
+      end
+
     end
   end
 end
